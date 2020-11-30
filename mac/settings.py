915b3e233dab3efe4443ebf8a46ae3be79a37b5c
Naman Jain <namanb139j@gmail.com>
2020-11-30 20:14:33 +0530

    push

--- mac/settings.py
+++ mac/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'g)@76*7no&(#-n@zw=2k#2%@d%ci-6sm7i3gbg1s%-p$ff0k)('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-   
+   'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- mac/urls.py
+++ mac/urls.py
@@ -25,5 +25,8 @@ urlpatterns = [
     path('shop/',include('shop.urls')),
     path('blog/',include('blog.urls')),
     path('',views.index),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
